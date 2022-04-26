f4c2160296ba34f70a902c2d279e2f7c0d131688
Alexandra Davis <alex.davis806@gmail.com>
2022-04-26 14:36:39 -0700

    commit tree 7845ca907c20a3ff8c0fcdd8bc5eed91ef6cac32

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,12 @@ require 'pry'
 
 class String
 
+  # def report_on_self
+  #   "Self is: #{self}"
+  # end
+
   def sentence?
-    
+    self.end_with?
   end
 
   def question?
@@ -17,4 +21,8 @@ class String
   def count_sentences
 
   end
+  binding.pry
 end
+
+my_string = String.new
+my_string = "Hi, my name is Sophie"
